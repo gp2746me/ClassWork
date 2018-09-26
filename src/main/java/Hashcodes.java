e5f08aed8f2373689f23984bac9a10faa418d67f
gp2746me <nilahanand@yahoo.com>
2018-09-26 10:16:48 -0500

    Hashcodes

---
+++ src/main/java/Hashcodes.java
@@ -0,0 +1,20 @@
+/**
+ * Created by gp2746me on 9/26/2018.
+ */
+public class Hashcodes {
+    public static void main(String[] args) {
+
+        String hello = "hello";
+        String helloExclamation = "hello!";
+        String helloCapitalH = "Hello";
+
+        System.out.println(hello.hashCode());
+        System.out.println(helloExclamation.hashCode());
+        System.out.println(helloCapitalH.hashCode());
+
+
+    }
+
+
+
+}
